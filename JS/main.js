045ed87cb74e0c731990eb9fbdb5e31c62ac1986
Ryanhansenswork <RyanhansensworkW@example.com>
2024-03-08 00:02:25 -0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://api.nasa.gov/planetary/apod?api_key=EsFcRfGoF3NjIzpRq2fRyFCJCYkfki5tPShmL4VL&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) 
+      .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+        document.querySelector('h2').innerText = data.title
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
