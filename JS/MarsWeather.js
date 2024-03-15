f1b1985fe49644534cb4569343658642f9cd7242
Ryanhansenswork <106726215+Ryanhansenswork@users.noreply.github.com>
2024-03-15 13:10:27 -0400

    Add files via upload

--- JS/MarsWeather.js
+++ JS/MarsWeather.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById('getWeatherBtn').addEventListener('click', function() {
     var sol = document.getElementById('selectSol').value;
     fetch('https://api.nasa.gov/insight_weather/?api_key=EsFcRfGoF3NjIzpRq2fRyFCJCYkfki5tPShmL4VL&feedtype=json&ver=1.0')
@@ -15,7 +16,24 @@ document.getElementById('getWeatherBtn').addEventListener('click', function() {
         })
         .catch(error => console.error('Error fetching data:', error));
 });
-
+*/
+document.getElementById('getWeatherBtn').addEventListener('click', function() {
+    var sol = document.getElementById('selectSol').value;
+    fetch('https://api.nasa.gov/insight_weather/?api_key=EsFcRfGoF3NjIzpRq2fRyFCJCYkfki5tPShmL4VL&feedtype=json&ver=1.0')
+        .then(response => response.json())
+        .then(data => {
+            if (data.hasOwnProperty('sol' + sol)) {
+                var solData = data['sol' + sol];
+                var solDate = new Date(solData.First_UTC);
+                document.getElementById('solDate').innerText = 'Sol Date: ' + solDate.toDateString();
+                document.getElementById('highTemp').innerText = 'High Temperature: ' + solData.AT.mx.toFixed(2) + ' °C';
+                document.getElementById('lowTemp').innerText = 'Low Temperature: ' + solData.AT.mn.toFixed(2) + ' °C';
+            } else {
+                alert('Nasa turned off their mars weather services :(');
+            }
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
 
 
 
